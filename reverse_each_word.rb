62be23075a58e93ba331171afac5c2b041383b02
Jasterix C <loune.calixte@gmail.com>
2019-04-08 21:49:31 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  arr = string.split(" ")
+  return arr.each {|x| x.reverse}
+end
+
+def reverse_each_word(string)
+  arr = string.split(" ")
+  arr.collect {|x| x.reverse}
+end
